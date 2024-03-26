d0775bad4c64707788bdd987d88945305f02abde
Mégane <megane.authemayou@gmail.com>
2024-03-26 18:34:09 +0100

    finish Quest 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,28 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = {
+    name: pokemonList[1].name,
+    imgSrc: pokemonList[1].imgSrc,
+  };
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Pokemon Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt="Pokemon Bulbasaur" />
+      ) : (
+        <p> ???? </p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
