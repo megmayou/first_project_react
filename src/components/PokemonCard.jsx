0854b9e8b3899ff39b1bcaabb56a76aaf38b0122
Mégane <megane.authemayou@gmail.com>
2024-03-27 12:16:16 +0100

    React Basics 04 - Props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,4 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = {
-    name: pokemonList[1].name,
-    imgSrc: pokemonList[1].imgSrc,
-  };
-
+function PokemonCard({ pokemon }) {
   return (
     <figure>
       {pokemon.imgSrc ? (
